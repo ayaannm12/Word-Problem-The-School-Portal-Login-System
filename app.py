9fed53852c08b49a62c5d27f4d04418541e53306
ayaannm12 <ayaanm5352@gmail.com>
2025-11-18 10:16:26 -0500

    aaa
    aaa

---
+++ app.py
@@ -0,0 +1,30 @@
+
+email = str(input("what is your email???")).strip()
+if "@" not in email:
+        email == False
+        print("email not valid there is no @ symbol!!")
+while email == False:
+        email = str(input("what is your email???")).strip()
+if "@" in email:
+    print("Your email is valid, thnak you!")
+    email == True
+if email == True:
+    while password == False:
+                password = str(input("what would you like your password to be??"))
+                if len(password) < 8:
+                        print("password is not valid, its too short 8 characters are the minimum!")
+                        password == False
+                if any(not char.isdigit() for char in password):
+                        print("Your password is not valid, you need atleast one number!")
+                        password == False
+                        if any(not char.isupper() for char in password):
+                                print("Password is not valid you need atleast one upper case nummber !")
+                                password == False
+if email == True and password == True:
+        print("they both are valid heres your info:")
+        print("Username", email)
+        print("password", password)
+        
+        
+                
+
